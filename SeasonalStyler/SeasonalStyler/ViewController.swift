4c3181dfe7392cece336e17dae60b89606edd284
Kiyomi <kkblackmun@ucdavis.edu>
2024-04-22 01:25:55 -0700

    init swift folders

---
+++ SeasonalStyler/SeasonalStyler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeasonalStyler
+//
+//  Created by Kiyomi Blackmun on 4/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
